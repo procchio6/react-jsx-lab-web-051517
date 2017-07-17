22bfff3a02a7047fc9c4c2d6fcdd8ae27f7ec165
Pat Rocchio <procchio6@gmail.com>
2017-07-17 13:22:21 -0400

    Done.

---
+++ src/components/RegistrationForm.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+
+class RegistrationForm extends Component {
+  render() {
+    return (
+      <form>
+        <input type='text'/>
+        <input type='password'/>
+        <button type='submit'></button>
+      </form>
+    )
+  }
+}
+
+export default RegistrationForm
---
+++ src/components/Webpage.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react'
+import FillerText from './FillerText'
+
+class Webpage extends Component {
+  render() {
+    return (
+      <div>
+        <p>The world's coolest webpage</p>
+        <FillerText/>
+        <FillerText/>
+      </div>
+    )
+  }
+}
+
+export default Webpage
